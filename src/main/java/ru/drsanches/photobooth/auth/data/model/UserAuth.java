76ec45eacf58cfec11136a85515470d9bc2c9026
Alexander <dr.sanches3@gmail.com>
2022-08-10 23:03:51 +0300

    Deleted unnecessary UserDetails implementation

--- src/main/java/ru/drsanches/photobooth/auth/data/model/UserAuth.java
+++ src/main/java/ru/drsanches/photobooth/auth/data/model/UserAuth.java
@@ -1,24 +1,21 @@
 package ru.drsanches.photobooth.auth.data.model;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import ru.drsanches.photobooth.common.token.data.Role;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Collection;
-import java.util.List;
 
-//TODO: Is it necessary to implement UserDetails?
+@Getter
 @Setter
 @ToString
 @Entity
 @Table(name="user_auth")
-public class UserAuth implements UserDetails {
+public class UserAuth {
 
     @Id
     @Column
@@ -43,55 +40,4 @@ public class UserAuth implements UserDetails {
 
     @Column(nullable = false)
     private Role role;
-
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    public String getSalt() {
-        return salt;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
 }
