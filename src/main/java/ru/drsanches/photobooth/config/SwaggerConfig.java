e6900ef8644ca29c9bccf8bfc9c9ed2a242d8f1c
Alexander <dr.sanches3@gmail.com>
2022-08-11 22:12:39 +0300

    Removed unnecessary todo

--- src/main/java/ru/drsanches/photobooth/config/SwaggerConfig.java
+++ src/main/java/ru/drsanches/photobooth/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//TODO: Add more response statuses
 //TODO: Fix headers on UI
 @Configuration
 public class SwaggerConfig {
